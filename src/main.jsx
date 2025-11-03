417d4fa2f87905cad0450c7afd867da8a47f80c6
tanvi183 <mneshat7@gmail.com>
2025-11-02 20:29:05 -0800

    Basic project setup: folder structure, route define

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layouts/RootLayout.jsx";
+import Home from "./components/Home/Home.jsx";
+import AllProducts from "./components/AllProducts/AllProducts.jsx";
+import Register from "./components/Register/Register.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'allProducts',
+        Component: AllProducts
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
